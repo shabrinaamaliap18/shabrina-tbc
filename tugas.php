e762b852056c4baa38ecb5bd9cc82acb60091c82
shabrinaamaliap18 <73974662+shabrinaamaliap18@users.noreply.github.com>
2021-03-19 14:35:04 +0700

    Add files via upload

---
+++ tugas.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+     <title>Tugas TBC</title>
+</head>
+
+<body>
+<div class="col-lg-6 col-md-7 col-8">
+                      <h2 class="my-5 pt-3" data-aos="fade-up" data-aos-delay="100">My Profile</h2>
+
+                      <div class="quote" data-aos="fade-up" data-aos-delay="200"></div>
+                      <div class="card">
+                      <div class="card-body">
+                      <p data-aos="fade-up" data-aos-delay="100">
+                        <strong>Shabrina Amalia Putri</strong>
+                        <br>
+                        <small>Perempuan</small><br>
+                        <small>Jln Semeru Lumajang, Jawa Timur</small>
+                      </p>
+                  
+                    </div>
+
+               </div>
+          </div>
+    
+
+
+<style>
+body{
+    
+    background: linear-gradient(150deg, var(--primary-color) 40%, var(--dark-color) 60%);
+    color : white;
+}
+
+</style>
